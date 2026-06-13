bf7192563e20d13bf5dcd4ff722bb2fee0432520
srq345274 <3277106612@qq.com>
2026-06-13 19:07:08 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route("/api/ping")
 def ping():
     print("收到前端访问请求")
-    return {"status":"ok"}
+    return {
+        "status": "ok",
+        "version": "v2"
+    }
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    print("CI/CD Test v2")
+    app.run(host="0.0.0.0", port=5000)
